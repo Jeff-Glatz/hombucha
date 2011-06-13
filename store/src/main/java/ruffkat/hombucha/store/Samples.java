3048de3e41c5ab4870f3a23a259330f5999b8af0
ruffkat <jeff@ruffkat.com>
2011-06-13 06:24:50 -0500

    Add volume as a sample

--- store/src/main/java/ruffkat/hombucha/store/Samples.java
+++ store/src/main/java/ruffkat/hombucha/store/Samples.java
@@ -5,8 +5,10 @@ import ruffkat.hombucha.model.Molarity;
 import ruffkat.hombucha.model.Sample;
 
 import javax.measure.quantity.Dimensionless;
+import javax.measure.quantity.Volume;
 
 public interface Samples {
     Sample<Molarity> ph(Ferment ferment);
     Sample<Dimensionless> specificGravity(Ferment ferment);
+    Sample<Volume> volume(Ferment ferment);
 }
--- store/src/main/java/ruffkat/hombucha/store/SamplesImpl.java
+++ store/src/main/java/ruffkat/hombucha/store/SamplesImpl.java
@@ -5,10 +5,12 @@ import ruffkat.hombucha.model.Molarity;
 import ruffkat.hombucha.model.Sample;
 
 import javax.measure.quantity.Dimensionless;
+import javax.measure.quantity.Volume;
 import java.util.Calendar;
 
 public class SamplesImpl implements Samples {
 
+    @Override
     public Sample<Molarity> ph(Ferment ferment) {
         Sample<Molarity> ph = new Sample<Molarity>();
         ph.setFerment(ferment);
@@ -16,10 +18,19 @@ public class SamplesImpl implements Samples {
         return ph;
     }
 
+    @Override
     public Sample<Dimensionless> specificGravity(Ferment ferment) {
         Sample<Dimensionless> sg = new Sample<Dimensionless>();
         sg.setFerment(ferment);
         sg.setTakenAt(Calendar.getInstance());
         return sg;
+    }
+
+    @Override
+    public Sample<Volume> volume(Ferment ferment) {
+        Sample<Volume> volume = new Sample<Volume>();
+        volume.setFerment(ferment);
+        volume.setTakenAt(Calendar.getInstance());
+        return volume;
     }
 }
