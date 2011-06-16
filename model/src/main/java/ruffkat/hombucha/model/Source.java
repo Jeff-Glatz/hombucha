c5f56b3204bcdce20a0317b85b05319df40bbf01
ruffkat <jeff@ruffkat.com>
2011-06-16 01:28:01 -0500

    Fix mapping

--- model/src/main/java/ruffkat/hombucha/model/Source.java
+++ model/src/main/java/ruffkat/hombucha/model/Source.java
@@ -4,7 +4,6 @@ import javax.persistence.Basic;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
@@ -16,7 +15,7 @@ public class Source
         implements Persistent {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE)
+    @GeneratedValue
     private Long oid;
 
     @Basic
